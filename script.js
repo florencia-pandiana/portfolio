f3af06188250ec92924d2275aa7403d94ea45622
flo-pandiana <florencia.pandiana@students.mq.edu.au>
2026-07-16 03:14:24 +1000

    Add folder icons to homescreen

--- script.js
+++ script.js
@@ -2,9 +2,7 @@
 
 function updateClock() {
   const now = new Date();
-
   const dateText = now.toLocaleDateString('en-GB', { day: '2-digit', month: 'long', year: 'numeric' });
-
   const hours = String(now.getHours()).padStart(2, '0');
   const minutes = String(now.getMinutes()).padStart(2, '0');
   const timeText = hours + ':' + minutes;
@@ -13,18 +11,43 @@ function updateClock() {
   document.getElementById('time').textContent = timeText;
 }
 
+/*---unlocking screen---*/
+
 updateClock();
 setInterval(updateClock, 10000);
 
-/*---tap to unlock---*/
 const lockscreen = document.getElementById('lockscreen');
 const homescreen = document.getElementById('homescreen');
+const folder = document.getElementById('file');
+// const folder1 = document.getElementById('whole');
 
-lockscreen.addEventListener('click', () => {
-  alert('clicked!');
-  lockscreen.classList.add('unlocking');
-  setTimeout(() =>{
-    lockscreen.style.display = 'none';
-    homescreen.style.display = 'block';
-  }, 400);
+document.addEventListener('click', (e) => {
+  const isLocked = homescreen.style.display === 'none';
+
+  if (isLocked) {
+    lockscreen.classList.add('unlocking');
+    folder.classList.add('docked');
+    setTimeout(() => {
+      lockscreen.style.display = 'none';
+      homescreen.style.display = 'block';
+      // folder1.style.display = 'block';
+    }, 400);
+  }
+});
+
+/*---locking screen---*/
+
+folder.addEventListener('click', (e) => {
+  const isLocked = homescreen.style.display === 'none';
+
+  if (!isLocked) {
+    e.stopPropagation();
+    lockscreen.classList.remove('unlocking');
+    folder.classList.remove('docked');
+    lockscreen.style.display = 'block';
+    setTimeout(() => {
+      homescreen.style.display = 'none';
+      folder1.style.display = 'none';
+    }, 400);
+  }
 });
